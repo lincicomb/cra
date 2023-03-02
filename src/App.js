8a14987c2612965bcd93e38423fefd008ab8a8a1
lincicomb <berkeleylincicome@gmail.com>
2023-03-02 12:50:38 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className='absolute w-[250px] h-[100px] bg-slate-400  bottom-0 right-0 grid place-content-center z-50'>
+          <div className="h-full">
+            <p className="text-white">This is the widget</p>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
