3fe20eed422d782218975f7581fb4be3d1f33f89
lincicomb <berkeleylincicome@gmail.com>
2023-03-02 13:21:45 -0400

    test2

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import cssText from 'bundle-text:./index.css';
 
+// inject <style> tag
+let style = document.createElement('style');
 const root = ReactDOM.createRoot(document.getElementById('widget'));
+style.textContent = cssText;
+root.appendChild(style);
+
 root.render(
   <React.StrictMode>
     <App />
